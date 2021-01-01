517c49801eda4ffa6e54a3e14166dcd8b0167d2e
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2021-01-01 16:41:17 +0100

    commit more often please

--- perspective_utils.py
+++ perspective_utils.py
@@ -5,16 +5,38 @@ Created on Fri May  1 15:18:40 2020
 @author: Hubert Bretonnière
 """
 from PIL import Image
-from random import randint
 import numpy as np
+import moviepy.video.io.ImageSequenceClip
+import re
+import glob
 
 
-def make_list_cube(image, distance=35, depth_factor=2, pix_size=2):
+def sort_nicely(liste):
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+    liste.sort(key=alphanum_key)
+    return liste
+
+
+def get_hex_color(x):
+    r = max(0, min(x[0], 255))
+    g = max(0, min(x[1], 255))
+    b = max(0, min(x[2], 255))
+    return "#{0:02x}{1:02x}{2:02x}".format(r, g, b)
+
+
+def make_list_cube(image, distance=70, depth_factor=3, pix_size=2):
 
     im = np.asarray(Image.open("data/images/"+image+str(".png")))  # Can be many different formats
+    if im.shape[2] == 4:
+        im = im[:, :, :3]
+    im = np.flip(im)
+    im = np.flip(im, axis=1)
     size_x = np.shape(im)[0]  # Get the width and hight of the image for iterating over
     size_y = np.shape(im)[1]
-    list_cube = [[0, 0, 0, 0], [0, 0, 0, 0]]  # on initialise a avec deux lignes pour ne pas avoir de problemes de dimension dans la boucle
+    list_cube = [[np.shape(im)[1]/13*4, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+
+    print(list_cube)
     size = 1
     # parcourt pixel de l'image reelle
     for x0 in range(0, size_x):
@@ -25,7 +47,8 @@ def make_list_cube(image, distance=35, depth_factor=2, pix_size=2):
                 y = r * (y0 - float(size_y) / 2.)
                 z = r * distance
                 p = float(r) * pix_size / 2.
-                new_a = [x, y, z, p]
+                c = im[x0, y0]
+                new_a = [x, y, z, p, c]
                 for s in range(1, size):
                     d = np.sqrt((x - (list_cube[s])[0])**2 + (y - (list_cube[s])[1])**2 + (z - (list_cube[s])[2])**2)
                     if float(d) < np.sqrt(2)*(r+(list_cube[s])[3]):  # if yes, cubes are intersecting then break
@@ -34,8 +57,10 @@ def make_list_cube(image, distance=35, depth_factor=2, pix_size=2):
                         continue
                 list_cube = np.vstack([list_cube, new_a])
                 size = size+1
-    file_out = open("data/list_objects/list_cube_" + str(image) + ".csv", "w")
-    for i in range(2, size+1):
-        file_out.write(str((list_cube[i])[0])+','+str((list_cube[i])[1])+','+str((list_cube[i])[2])+','+str((list_cube[i])[3])+'\n')
+    np.save("data/list_objects/list_cube_" + str(image) + ".npy", list_cube)
 
-    file_out.close
+
+def make_movie(name, fps):
+    image_files = [img for img in sort_nicely(glob.glob('data/rendered/'+name+'*.png'))]
+    clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
+    clip.write_videofile('data/rendered/'+name+'_video.mp4')
--- script_blender.py
+++ script_blender.py
@@ -1,15 +1,29 @@
 import csv
 import bpy
 import numpy as np
+import sys
 
+nargv = len(sys.argv) - 1
+nb_frames = int(sys.argv[nargv])
+name = sys.argv[nargv-1]
 bpy.ops.render.render(write_still=True)
-file = csv.reader(open("data/list_objects/list_cube_smiley.csv"))
-cube_mat = bpy.data.materials['cube_material']
+file = np.load("data/list_objects/list_cube_"+name+".npy", allow_pickle=True)
 
-for i, line in enumerate(file):
-    bpy.ops.mesh.primitive_cube_add(size=float(line[3])/4, location=(float(line[1])/4, float(line[2])/4, float(line[0])/4))
+# from normal scale to blender scale
+reduce = file[0][0]
+
+
+print(reduce)
+for i in range(2, file.shape[0]):
+    if i == 0:
+        continue
+    bpy.ops.mesh.primitive_cube_add(size=float(file[i][3])/reduce, location=(float(file[i][1])/reduce, float(file[i][2])/reduce, float(file[i][0])/reduce))
     cube_i = bpy.context.selected_objects[0]
     cube_i.name = 'cube_{}'.format(i)
+    color = tuple(reversed(tuple(file[i][4]/255)))+(1,)
+    cube_mat = bpy.data.materials.new("PKHG")
+    cube_mat.diffuse_color = color
+    cube_mat.roughness = 1
     cube_i.data.materials.append(cube_mat)
 
 
@@ -19,24 +33,23 @@ bpy.ops.object.select_all(action='DESELECT')
 
 scene = bpy.data.scenes["Scene"]
 
-nb_frames = 10
 fov = 200.0
 scene.camera.rotation_mode = 'XYZ'
 
-rxs = np.linspace(75, 85, nb_frames)*np.pi/180
+rxs = np.linspace(70, 85, nb_frames)*np.pi/180
 ry = 0
 rzs = np.linspace(np.pi+np.pi/6, 2*np.pi, nb_frames)
 scene.camera.rotation_euler[1] = ry
 
-zs = np.linspace(7, 3, nb_frames)
+zs = np.linspace(6, 2, nb_frames)
 
 frame = 0
-ymax = 25
-ymin = -10
+ymax = 40
+ymin = -2
 for i, angle in enumerate(np.linspace(np.pi+np.pi/6, 2*np.pi, nb_frames)):
     frame += 1
-    y = (ymax+ymin)+ymax*np.cos(angle+np.pi)
-    x = 25*np.sin(angle)
+    y = ymax*np.cos(angle+np.pi)
+    x = 18*np.sin(angle)
 
     # Set camera rotation in euler angles
     rz = rzs[i]
@@ -48,12 +61,15 @@ for i, angle in enumerate(np.linspace(np.pi+np.pi/6, 2*np.pi, nb_frames)):
     scene.camera.location.z = zs[i]
     scene.camera.location.y = y
     scene.camera.location.x = x
-    bpy.context.scene.render.filepath = './data/rendered/frame_{}'.format(frame)
+    bpy.context.scene.render.filepath = './data/rendered/'+name+'_frame_{}'.format(frame)
     bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.ops.render.render(write_still=True)
 
-nb_frames_tr = 5
-zs_tr = np.linspace(3, 0, nb_frames_tr)
+if nb_frames > 16:
+    nb_frames_tr = nb_frames/8
+else:
+    nb_frames_tr = 2
+zs_tr = np.linspace(2, 0, nb_frames_tr)
 rxs_tr = np.linspace(85, 90, nb_frames_tr)*np.pi/180
 
 for i, y in enumerate(np.linspace(ymin, 0, nb_frames_tr)):
@@ -62,6 +78,6 @@ for i, y in enumerate(np.linspace(ymin, 0, nb_frames_tr)):
     scene.camera.location.z = zs_tr[i]
     scene.camera.rotation_euler[0] = rxs_tr[i]
 
-    bpy.context.scene.render.filepath = './data/rendered/frame_{}'.format(frame)
+    bpy.context.scene.render.filepath = './data/rendered/'+name+'_frame_{}'.format(frame)
     bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.ops.render.render(write_still=True)
