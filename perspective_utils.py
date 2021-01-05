41c867e58185fd3346292da99e54e8bb92dc9497
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2021-01-05 15:57:53 +0100

    comments on the codes'

--- perspective_utils.py
+++ perspective_utils.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Fri May  1 15:18:40 2020
-
 @author: Hubert Bretonnière
 """
 from PIL import Image
@@ -12,6 +10,9 @@ import glob
 
 
 def sort_nicely(liste):
+    ''' sort list of string combining letters and numbers.
+    adapted from https://gist.github.com/limed/473a498641bbc7761a20 '''
+
     convert = lambda text: int(text) if text.isdigit() else text
     alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
     liste.sort(key=alphanum_key)
@@ -19,6 +20,7 @@ def sort_nicely(liste):
 
 
 def get_hex_color(x):
+    'Transform a RBG color to its hexadecimal code'
     r = max(0, min(x[0], 255))
     g = max(0, min(x[1], 255))
     b = max(0, min(x[2], 255))
@@ -26,39 +28,69 @@ def get_hex_color(x):
 
 
 def make_list_cube(image, distance=70, depth_factor=3, pix_size=2):
+    ''' 
+    Creates a list of cubes in perspective representing an input 2D image.
+    Args : 
+            - image : a png or jpeg RGB (or RGBA) image
+            - distance : the distance of the closest cube from the zero point perspective.
+                         Need to be big enough for the image to be completely in the field
+                         of view at the 0 point perspective
+            - depth_factor : the maximum ratio between the closest and the farest cube distance.
+                             ( or similarly between the min and max sizes of the cubes)
+            - pix_size : The smallest size of the cubes. ( I think it need to be hard coded)
+    Returns :
+            - a numpy array containing for each non white pixel of the input image
+              the (x, y, z) coordinates and its size (bigger if farther, smaller if closer)
+              along with its RGB color.
+              Also save the array to a .npy file with the name of the image.
+    '''
 
     im = np.asarray(Image.open("data/images/"+image+str(".png")))  # Can be many different formats
-    if im.shape[2] == 4:
+
+    if im.shape[2] == 4:  # We don't use the transparency of a RGBA image
         im = im[:, :, :3]
+
+    ''' ======== Pre-processing======== '''
+    ''' Because of projection issue, we need to flip vertically and horizontaly the image'''
     im = np.flip(im)
     im = np.flip(im, axis=1)
-    size_x = np.shape(im)[0]  # Get the width and hight of the image for iterating over
+
+    size_x = np.shape(im)[0]  
     size_y = np.shape(im)[1]
     list_cube = [[np.shape(im)[1]*12, 0, 0, 0, 0], [0, 0, 0, 0, 0]] # intialize the list with shape infos
-    size = 1
-    # parcourt pixel de l'image reelle
+    nb_cubes = 1
+
+    ''' ======== Creation of the perspective ========= '''
+    ''' We iterate through all the pixels, along x and y'''
     for x0 in range(0, size_x):
         for y0 in range(0, size_y):
-            if list(im[x0, y0]) != [255, 255, 255]:
-                r = np.random.uniform(1, depth_factor)  # randint(100, 300)/100. #r=facteur de reduction aleatoire (entre 100 et 300% de la taille du pixel de base)
+            if list(im[x0, y0]) != [255, 255, 255]:  # continue if the pixel is white
+                r = np.random.uniform(1, depth_factor) # random depth factor for the current pixel
+                ''' The 3D coordinates and the size of the cube are conditionned to the depth factor '''
                 x = r * (x0 - float(size_x) / 2.)
                 y = r * (y0 - float(size_y) / 2.)
                 z = r * distance
-                p = float(r) * pix_size / 2.
+                p = float(r) * pix_size / 2.  # size of the cube (side)
                 c = im[x0, y0]
-                new_a = [x, y, z, p, c]
-                for s in range(1, size):
+                cube = [x, y, z, p, c]
+                ''' We need do check that there is no conflict between the cubes (no interscetion). 
+                To do so, for each new cube, we see if it's intersecting it. We retry a new random depth factor untill
+                we have no conflict. 
+                To do : Is must have a more elegant way to do that... '''
+                for s in range(1, nb_cubes):
                     d = np.sqrt((x - (list_cube[s])[0])**2 + (y - (list_cube[s])[1])**2 + (z - (list_cube[s])[2])**2)
                     if float(d) < np.sqrt(2)*(r+(list_cube[s])[3]):  # if yes, cubes are intersecting then break
                         break
                     else:
                         continue
-                list_cube = np.vstack([list_cube, new_a])
-                size = size+1
+                list_cube = np.vstack([list_cube, cube])
+                nb_cubes += 1
     np.save("data/list_objects/list_cube_" + str(image) + ".npy", list_cube)
+    return list_cube
 
 
 def make_movie(name, fps):
+    ''' Creates a video from frames'''
     image_files = [img for img in sort_nicely(glob.glob('data/rendered/'+name+'/'+name+'*.png'))]
     clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
     clip.write_videofile('data/rendered/'+name+'/'+name+'_video.mp4')
--- script_blender.py
+++ script_blender.py
@@ -1,45 +1,85 @@
+'''
+This script will be run by blender using the python 
+interface API (https://docs.blender.org/api/current/info_overview.html).
+It allows us to create a scene with the cubes in perspective. To
+have a nice view, it creates the cubes from the list calculated in
+do_cubes.py directly in a scene already made (museum.blend).
+Then, we use the API to create the frames of the final video,
+by moving the camera around the cubes following a 3d ellipse,
+and rendering the scene at each step.
+
+The video will be assembled in do_video.py
+
+@author: Hubert Bretonnière
+
+'''
+
 import os
 import bpy
 import numpy as np
 import sys
 
-nargv = len(sys.argv) - 1 
+''' initialisation '''
+nargv = len(sys.argv) - 1
 nb_frames = int(sys.argv[nargv])
 name = sys.argv[nargv-1]
-file = np.load("data/list_objects/list_cube_"+name+".npy", allow_pickle=True)
+list_cubes = np.load("data/list_objects/list_cube_"+name+".npy", allow_pickle=True)
 
 bpy.ops.render.render(write_still=True)
 
 # from normal scale to blender scale
 reduce = 12
 
+# We will save the images in a folder with the name of the original image
 if not os.path.exists('data/rendered/'+name):
     os.makedirs('data/rendered/'+name)
 
-print(reduce)
-for i in range(2, file.shape[0]):
-    if i == 0:
-        continue
-    bpy.ops.mesh.primitive_cube_add(size=float(file[i][3])/reduce, location=(float(file[i][1])/reduce, float(file[i][2])/reduce, float(file[i][0])/reduce))
+''' =======================
+    Creation of the cubes
+    ========================
+We loop for all the cubes of the list, and create a blender cube object,
+with the right position and size. We rename it, and change its color by
+creating a new blender material
+To do : re-use a material if the color has been already seen'''
+
+for i in range(2, list_cubes.shape[0]):
+    bpy.ops.mesh.primitive_cube_add(size=float(list_cubes[i][3])/reduce, location=(float(list_cubes[i][1])/reduce, float(list_cubes[i][2])/reduce, float(list_cubes[i][0])/reduce))
     cube_i = bpy.context.selected_objects[0]
     cube_i.name = 'cube_{}'.format(i)
-    color = tuple(reversed(tuple(file[i][4]/255)))+(1,)
+    color = tuple(reversed(tuple(list_cubes[i][4]/255)))+(1,)
     cube_mat = bpy.data.materials.new("RGB")
     cube_mat.diffuse_color = color
-    cube_mat.roughness = 1
+    cube_mat.roughness = 1  # make the material less shiny
     cube_i.data.materials.append(cube_mat)
 
 
-if bpy.context.object.mode == 'EDIT':
-    bpy.ops.object.mode_set(mode='OBJECT')
-bpy.ops.object.select_all(action='DESELECT')
+# if bpy.context.object.mode == 'EDIT':
+#     bpy.ops.object.mode_set(mode='OBJECT')
+# bpy.ops.object.select_all(action='DESELECT')
+''' ==============================
+        Creation of the frames
+    ==============================
+The image is creating on the [x, z] plane, and the perspewctive is following the y axis. The perfect perspective is at
+the origin (0,0,0)
+To do a nice video, we want to move the camera around the cubes, from the back to the final position.
+To do so, we move the camera along half an ellipse from behind the cubes (ymax=25) to a bit behind the cube (ymin=-5)
+We rotate the facing direction of the camera along the z axis so it always faces the cubes during the movement.
+We also incline a bit the ellipse by changing the height of the camera (z=6 to z=2), and rotating it a bit
+along the x axis so it faces the cubes.
+Finally, we add a last translation from a point a bit behind and above the 0 point to the zero point to have
+the perfect perspective.
+'''
+
+
+''' ====== Ellipse ======= '''
+'''Initilisation of the camera movements'''
 
 scene = bpy.data.scenes["Scene"]
 fov = 200.0
 scene.camera.rotation_mode = 'XYZ'
 
-rot_x = np.linspace(75, 85, nb_frames)*np.pi/180  # from a bit to the floor to parallel
-rot_y = 0
+rot_x = np.linspace(75, 85, nb_frames)*np.pi/180  # begin facing a bit the floor and going to more parralle to the floor
+rot_y = 0  # no rotation along the y axis
 rot_z = np.linspace(180, 360, nb_frames)*np.pi/180  # from facing the wall behind to facing the cubes
 scene.camera.rotation_euler[1] = rot_y
 
@@ -49,10 +89,11 @@ frame = 0
 ymax = 38
 ymin = -10
 xmax = 18
-for i, angle in enumerate(np.linspace(np.pi, 0, nb_frames)):
+''' The (x,y) ellipse coordinates are parameterized by a single angle, from 360 to 180, with nb_frames steps'''
+for i, angle in enumerate(np.linspace(2*np.pi, np.pi, nb_frames)):
     frame += 1
-    y = (ymax+ymin)/2 + (ymax-ymin)/2*np.cos(angle+np.pi)
-    x = xmax*np.sin(angle+np.pi)
+    y = (ymax+ymin)/2 + (ymax-ymin)/2*np.cos(angle)
+    x = xmax*np.sin(angle)
 
     # Set camera rotation in euler angles
     scene.camera.rotation_euler[2] = rot_z[i]
@@ -62,22 +103,29 @@ for i, angle in enumerate(np.linspace(np.pi, 0, nb_frames)):
     scene.camera.location.z = translat_z[i]
     scene.camera.location.y = y
     scene.camera.location.x = x
+
+    # Render and save the current frame
     bpy.context.scene.render.filepath = './data/rendered/'+name+'/'+name+'_frame_{}'.format(frame)
     bpy.context.scene.render.image_settings.file_format = 'PNG'
     bpy.ops.render.render(write_still=True)
+
+''' ====== Final Translations ======= '''
+''' The number of steps is conditioned on the number used for the ellipse
+To do : give better meaning of 'nb_frames' '''
 
 if nb_frames > 16:
     nb_frames_tr = nb_frames/8
 else:
     nb_frames_tr = 2
-zs_tr = np.linspace(2, 0, nb_frames_tr)
-rot_x_tr = np.linspace(85, 90, nb_frames_tr)*np.pi/180
+
+zs_tr = np.linspace(2, 0, nb_frames_tr)   # from a bit above to 0
+rot_x = np.linspace(85, 90, nb_frames_tr)*np.pi/180  # from a bit facing the floor to parrallel to the flooor
 
 for i, y in enumerate(np.linspace(ymin, 0, nb_frames_tr)):
     frame += 1
     scene.camera.location.y = y
     scene.camera.location.z = zs_tr[i]
-    scene.camera.rotation_euler[0] = rot_x_tr[i]
+    scene.camera.rotation_euler[0] = rot_x[i]
 
     bpy.context.scene.render.filepath = './data/rendered/'+name+'/'+name+'_frame_{}'.format(frame)
     bpy.context.scene.render.image_settings.file_format = 'PNG'
