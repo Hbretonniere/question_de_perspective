1a7b554fcf1a9777e817858a65f02e1abfa5926e
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2021-02-13 16:08:46 +0000

    good colors, im save as png

--- pixel_art.py
+++ pixel_art.py
@@ -3,6 +3,8 @@ import matplotlib as mpl
 from matplotlib import gridspec
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
+
 
 class pixel_art:
     def __init__(
@@ -13,6 +15,7 @@ class pixel_art:
     ):
         self.size = size
         self.canevas = fig.add_subplot(gs[0])
+        self.canevas.set_title('Left click to paint with the selected color. \n Right click (or ctrl click) to save.', fontsize=7)
         self.canevas.set_xticks(np.arange(self.size))
         self.canevas.set_yticks(np.arange(self.size))
         self.canevas.set_xticklabels([""]*self.size)
@@ -20,19 +23,23 @@ class pixel_art:
 #         self.canevas.set_xlim([0, size])
         
         self.show_palette = fig.add_subplot(gs[1])
+        self.show_palette.set_title('Left click on the color to select it.', fontsize=7)
         self.show_palette.set_xticks(np.arange(3))
         self.show_palette.set_yticks(np.arange(3))
         self.show_palette.set_xticklabels([""]*3)
         self.show_palette.set_yticklabels([""]*3)        
-        self.image = np.zeros((self.size, self.size))
-        self.palette = np.arange(9)
-        self.palette = self.palette.reshape(3, 3)
+        self.image = np.zeros((self.size, self.size, 3)) + (255, 255, 255)
+        self.image = self.image.astype(np.uint8)
+        self.palette = np.asarray([[255, 0, 0], [0, 255, 0], [0, 0, 255],
+                        [253, 173, 11], [125, 28, 187], [28, 187, 183],
+                        [233, 249, 35], [197, 95, 8], [255, 255, 255]])
+        self.palette = self.palette.reshape(3, 3, 3)
         
-        self.cmap = mpl.colors.ListedColormap(['white', 'red', 'green', 'blue', 'cyan', 'orange', 'black', 'purple', 'yellow'])
+#         self.cmap = mpl.colors.ListedColormap(['white', 'red', 'green', 'blue', 'cyan', 'orange', 'black', 'purple', 'yellow'])
         self.canevas.set_aspect('equal')
-        self.canevas.imshow(self.image, vmin=0, vmax=9, cmap=self.cmap, extent=[-1, self.size-1, -1, self.size-1], origin='lower')
+        self.canevas.imshow(self.image, vmin=0, vmax=9, extent=[-1, self.size-1, -1, self.size-1], origin='lower')
         self.canevas.grid()
-        self.show_palette.imshow(self.palette, vmin=0, vmax=9, cmap=self.cmap, extent=[-1, 2, -1, 2], origin='lower')
+        self.show_palette.imshow(self.palette, vmin=0, vmax=9, extent=[-1, 2, -1, 2], origin='lower')
         fig.canvas.mpl_connect('button_press_event', self)
         self.couleur = 0
 
@@ -44,15 +51,21 @@ class pixel_art:
         if event.button == 1:
             if ax == self.show_palette:
                 self.couleur = self.palette[ceil(click_y), ceil(click_x)]
+                plt.suptitle(" ", x=0.5, y=0.9)
 
             else :
 #                 self.canevas.set_title(ax)
                 self.image[ceil(click_y), ceil(click_x)] = self.couleur
-                self.canevas.imshow(self.image, cmap=self.cmap, vmin=0, vmax=9,
+                self.canevas.imshow(self.image, vmin=0, vmax=9,
                                     extent=[-1, self.size-1, -1, self.size-1], origin='lower')
+                plt.suptitle(" ", x=0.5, y=0.9)
                 plt.draw()
         if event.button == 3:
             np.save('custom_image.npy', self.image)
+            im = Image.fromarray(self.image)
+            im.save("custom_image.png")
+            plt.suptitle("SAVED !", x=0.5, y=0.9, color='red')
+            plt.draw()
 
             
 fig = plt.figure(figsize=(5, 5), constrained_layout=True)
