802aabbd771b3317e4e52c794c497c4e75fea1cd
Hubert Bretonniere <hubert.bretonniere@ias.u-psud.fr>
2021-06-16 17:03:51 +0100

    save with buttons

--- pixel_art.py
+++ pixel_art.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import os
+from matplotlib.widgets import Button
 
 
 class pixel_art:
@@ -15,8 +16,7 @@ class pixel_art:
         self.size = size
 
         self.canevas = fig.add_subplot(gs[0])
-        self.canevas.set_title('Left click to paint with the selected color. \n Right click (or ctrl click) to save'+\
-                                '\n Double click to launch the perspective rendering !', fontsize=7)
+        self.canevas.set_title('Left click to paint the pixel \n with the color selected on the Canvas.', fontsize=10)
         self.canevas.set_xticks(np.arange(self.size))
         self.canevas.set_yticks(np.arange(self.size))
         self.canevas.set_xticklabels([""]*self.size)
@@ -44,23 +44,17 @@ class pixel_art:
         fig.canvas.mpl_connect('button_press_event', self)
         self.couleur = 0
 
-#         cbar.ax.set_ticks([])
+        ax_save_button = plt.axes([0.65, 0.2, 0.15, 0.1])
+        ax_launch_button = plt.axes([0.82, 0.2, 0.15, 0.1])
+        self.save_button = Button(ax_save_button, 'SAVE', color='gray', hovercolor='red')
+        self.launch_button = Button(ax_launch_button, 'LAUNCH', color='gray', hovercolor='red')
+
     def __call__(self, event):
         click_x = event.xdata
         click_y = event.ydata
         ax = event.inaxes
-        if event.button == 1:
-            if ax == self.show_palette:
-                self.couleur = self.palette[ceil(click_y), ceil(click_x)]
-                plt.suptitle(" ", x=0.5, y=0.9)
 
-            else:
-                self.image[ceil(click_y), ceil(click_x)] = self.couleur
-                self.canevas.imshow(self.image, vmin=0, vmax=9,
-                                    extent=[-1, self.size-1, -1, self.size-1], origin='lower')
-                plt.suptitle(" ", x=0.5, y=0.92)
-                plt.draw()
-        if event.button == 3:
+        def save_drawing(val):
             im_to_save = np.copy(self.image)  # We need to inver the R and B channels
             im_to_save[:, :, 0] = self.image[:, :, 2]  # B to R
             im_to_save[:, :, 2] = self.image[:, :, 0]  # R to B
@@ -70,11 +64,26 @@ class pixel_art:
             im.save("data/images/custom_image.png")
             plt.suptitle("SAVED !", x=0.5, y=0.92, color='red')
             plt.draw()
-
-        if event.dblclick:
-            plt.suptitle("The video is ready, you can quit to see it !", x=0.5, y=0.92, color='red')
+        
+        def save_and_launch(val):
+            plt.suptitle("Wait for it !", x=0.5, y=0.92, color='red')
             plt.draw()
             os.system("sh main.sh custom_image 10 10")
+
+        self.save_button.on_clicked(save_drawing)
+        self.launch_button.on_clicked(save_and_launch)
+
+        if event.button == 1:
+            if ax == self.show_palette:
+                self.couleur = self.palette[ceil(click_y), ceil(click_x)]
+                plt.suptitle(" ", x=0.5, y=0.9)
+
+            if ax == self.canevas:
+                self.image[ceil(click_y), ceil(click_x)] = self.couleur
+                self.canevas.imshow(self.image, vmin=0, vmax=9,
+                                    extent=[-1, self.size-1, -1, self.size-1], origin='lower')
+                plt.suptitle(" ", x=0.5, y=0.92)
+                plt.draw()
 
 
 fig = plt.figure(figsize=(5, 5), constrained_layout=True)
